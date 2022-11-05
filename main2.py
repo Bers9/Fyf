8549112270990833d7e21221402f056993c1fd78
Bers9 <maja898967@gmail.com>
2022-11-05 18:52:42 +0300

    Задание 1

--- main2.py
+++ main2.py
@@ -4,5 +4,5 @@ for i in range(n):
         if i == 0 or j == 0 or i == n-1 or j == n-1:
             print("*",end="")
         else:
-            print(" ",end="")
+            print("*",end="")
     print("")
